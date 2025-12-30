e3fa750fcc17b84f1950e86a4657b4f5def58ed2
MarkeloPuangpoo <drvvdre@gmail.com>
2025-12-31 00:47:10 +0700

    Remove `allowedDevOrigins` configuration from Next.js.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    "http://localhost:3000",
-    "http://192.168.1.108:3000",
-    "http://169.254.83.107:3000",
-  ],
 };
 
 export default nextConfig;
